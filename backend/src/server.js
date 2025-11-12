1d91073043b4c040e637a0789b10ada468e57052
Vivek Rawat <vivekrawatyt01@gamil.com>
2025-11-12 14:21:20 +0530

    Fix missing stream.js import in inngest.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,7 @@ import { connectDB } from "./lib/db.js";
 import cors from "cors";
 import {serve} from "inngest/express"
 import { inngest } from "./lib/inngest.js";
+import { inngest, functions } from "./lib/inngest.js";
 
 const app = express();
 
