ed98e77521dc8467d450b6734072731ebefb073b
Vivek Rawat <vivekrawatyt01@gamil.com>
2025-11-13 20:30:25 +0530

    Fix dashboard page filename casing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,25 @@
 import { useState } from "react";
+import { useUser } from "@clerk/clerk-react";
 import HomePage from "./pages/HomePage";
+import DashboardPage from "./pages/DashboardPage";
+// import ProblemPage from "./pages/ProblemPage";
 import { Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import { Navigate } from "react-router";
 
 function App() {
+ const { isSignedIn, isLoaded } = useUser();
+
+  // this will get rid of the flickering effect
+  if (!isLoaded) return null;
+
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />} />
+        <Route path="/dashboard" element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />} />
+           
+        {/* <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />} /> */}
       </Routes>
 
       <Toaster toastOptions={{ duration: 3000 }} />
